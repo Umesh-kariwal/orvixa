b0b750c807ed068e15ed991a8a5dc3581389efc5
Umesh-kariwal <umesh.kariwal@gmail.com>
2026-08-13 15:03:58 +0530

    Fix Bug: Remove IS_EXTENSION guard from play_on_youtube case so YouTube opens 100% of the time in Web Mode and Extension Mode

--- src/hooks/desktopActions.ts
+++ src/hooks/desktopActions.ts
@@ -462,7 +462,7 @@ export async function executeVoiceAction(action: VoiceAction): Promise<string | 
       return action.response || null;
 
     case 'play_on_youtube':
-      if (IS_EXTENSION && action.query) {
+      if (action.query) {
         await autoPlayYouTubeVideo(action.query);
       }
       return action.response || null;
